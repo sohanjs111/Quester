dd643abfbe8603108ef57da1a77c51ab9522a762
Sohan Saldanha <sohanjs111@gmail.com>
2022-07-10 15:04:14 +0200

    Inital front end

---
+++ scripts.js
@@ -0,0 +1,9 @@
+<script>
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, a;
+  input = document.getElementById('search');
+  filter = input.value.toLowerCase();
+  ul = document.getElementById("myUL");
+}
+</script>
