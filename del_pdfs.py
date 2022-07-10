0b654fdab341ab984ab86125a61c7fe960602518
Sohan Saldanha <sohanjs111@gmail.com>
2022-07-10 12:09:16 +0200

    added delete file program

---
+++ del_pdfs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 10 02:36:02 2022
+
+@author: sohanjs
+"""
+
+import os
+import glob
+
+files = glob.glob("*.pdf")
+
+# Get the current working directory
+cwd = os.getcwd()
+print("Current working directory: {0}".format(cwd))
+
+for f in files:
+    os.system(f'rm -rf *.pdf')
